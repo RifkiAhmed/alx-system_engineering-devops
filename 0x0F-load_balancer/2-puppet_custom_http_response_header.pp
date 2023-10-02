eac8d2f134e8b4a20b6b7c70d44323330ec58564
ahmed <rifkiahmed.91@gmail.com>
2023-10-02 22:06:56 +0000

    Configure a brand new Ubuntu machine

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,7 +1,6 @@
 # Install and configure Nginx web server with Puppet
-
 exec {'apt_update':
-  command     => 'sudo apt update',
+  command     => 'sudo apt-get update',
   path        => ['/usr/bin', '/usr/sbin'],
   refreshonly => true,
 }
@@ -11,28 +10,21 @@ package {'nginx':
   require => Exec['apt_update'],
 }
 
-service {'nginx':
-  ensure  => 'running',
-  enable  => true,
-  require => Package['nginx'],
-}
-
-file {'/var/www/html/index.html':
-  ensure  => file,
-  content => 'Hello World!',
-  require => Package['nginx'],
-}
-
 exec {'redirection':
-  command => "sed -i '/^server {/a\\ \\n\\tlocation /redirect_me { return 301 https:\\/\\/www.google.com\\/;}\\n' /etc/nginx/sites-available/default",
+  command => "sed -i '/^server {/a\\ \\n\\tlocation /redirect_me {\
+return 301 https:\\/\\/www.google.com\\/;}' /etc/nginx/sites-available/default",
   path    => ['/usr/bin', '/usr/sbin'],
   require => Package['nginx'],
-  notify  => Service['nginx'],
 }
 
 exec {'header':
   command => "sed -i '/^server {/a\\ \\n\\tadd_header X-Served-By ${hostname};' /etc/nginx/sites-available/default",
   path    => ['/usr/bin', '/usr/sbin'],
-  require => Package['nginx'],
-  notify  => Service['nginx'],
+  require => Exec['redirection'],
+}
+
+exec {'restart':
+  command => 'sudo service nginx restart',
+  path    => ['/usr/bin', '/usr/sbin'],
+  require => Exec['header'],
 }
