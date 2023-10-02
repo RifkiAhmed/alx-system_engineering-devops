17c9dc68c072965d39cdffb2d8a622f922beb632
ahmed <rifkiahmed.91@gmail.com>
2023-10-02 22:11:08 +0000

    Configure a brand new Ubuntu machine

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,4 +1,5 @@
 # Install and configure Nginx web server with Puppet
+
 exec {'apt_update':
   command     => 'sudo apt-get update',
   path        => ['/usr/bin', '/usr/sbin'],
@@ -10,17 +11,10 @@ package {'nginx':
   require => Exec['apt_update'],
 }
 
-exec {'redirection':
-  command => "sed -i '/^server {/a\\ \\n\\tlocation /redirect_me {\
-return 301 https:\\/\\/www.google.com\\/;}' /etc/nginx/sites-available/default",
+exec {'header':
+  command => "sed -i '/^server {/a\\ \\n\\tadd_header X-Served-By \"${hostname}\";' /etc/nginx/sites-available/default",
   path    => ['/usr/bin', '/usr/sbin'],
   require => Package['nginx'],
-}
-
-exec {'header':
-  command => "sed -i '/^server {/a\\ \\n\\tadd_header X-Served-By ${hostname};' /etc/nginx/sites-available/default",
-  path    => ['/usr/bin', '/usr/sbin'],
-  require => Exec['redirection'],
 }
 
 exec {'restart':
