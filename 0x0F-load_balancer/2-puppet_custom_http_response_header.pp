1ecdc486901cd6869d09cc8cef37c3ea238ce31a
ahmed <rifkiahmed.91@gmail.com>
2023-10-03 01:33:35 +0000

    Add /bin directory for using sed command on Ubuntuwq 16.04

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,25 +1,24 @@
 # Install and configure Nginx web server with Puppet
 
 exec {'apt_update':
-  command     => 'apt update',
-  path        => ['/usr/bin', '/usr/sbin'],
-  refreshonly => true,
+  command  => 'apt update',
+  provider => shell,
 }
 
 package {'nginx':
-  ensure  => 'installed',
-  require => Exec['apt_update'],
-}
-
-service {'nginx':
-  ensure  => 'running',
-  enable  => true,
-  require => Package['nginx'],
+  ensure   => 'installed',
+  provider => apt,
+  require  => Exec['apt_update'],
 }
 
 exec {'header':
-  command => "sed -i '/^\\troot \\/var\\/www\\/html;/a\\ \\n\\tadd_header X-Served-By \"$hostname\";' /etc/nginx/sites-available/default",
+  command => "sed -i '/listen 80 default_server;/a\\ \\n\\tadd_header X-Served-By $hostname;' /etc/nginx/sites-available/default",
   path    => ['/bin', '/usr/bin', '/usr/sbin'],
   require => Package['nginx'],
-  notify  => Service['nginx'],
+}
+
+exec {'restart':
+  command  => 'sudo service nginx restart',
+  provider => shell,
+  require  => Exec['header'],
 }
